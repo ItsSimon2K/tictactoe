c67212f0477d1dc16fd503a21e6a2c598432706e
Simon2K <simon_sim88@yahoo.com>
2020-04-23 14:16:11 +0800

    Add Variables and draw 3 x 3 grid

--- tictactoe.rb
+++ tictactoe.rb
@@ -11,10 +11,31 @@ class GameWindow < Gosu::Window
 
   def initialize
     super(WIN_WIDTH, WIN_HEIGHT, false)
+
+    # Player's Turn
+    @playerTurn = "O" 
+
+    @isGameEnd = false
+
+    @board = ["-", "-", "-",
+              "-", "-", "-",
+              "-", "-", "-"]
+    
+    # Top right Coords of each button
+    @buttonPos = [[200, 95],[280, 95],[360, 95],
+                  [200, 175],[280, 175],[360, 175],
+                  [200, 255],[280, 255],[360, 255]]
+    
+    @buttonWidth = 80
   end
 
   def draw
+    draw_rect(200, 95, 240, 240, Gosu::Color::RED, ZOrder::BOTTOM)
 
+    draw_line(200, 176, Gosu::Color::BLACK, 440, 176, Gosu::Color::BLACK, ZOrder::MIDDLE, mode=:default)
+    draw_line(200, 256, Gosu::Color::BLACK, 440, 256, Gosu::Color::BLACK, ZOrder::MIDDLE, mode=:default)
+    draw_line(281, 95, Gosu::Color::BLACK, 281, 335, Gosu::Color::BLACK, ZOrder::MIDDLE, mode=:default)
+    draw_line(361, 95, Gosu::Color::BLACK, 361, 335, Gosu::Color::BLACK, ZOrder::MIDDLE, mode=:default)
   end
 
   def needs_cursor?
@@ -22,9 +43,11 @@ class GameWindow < Gosu::Window
   end
 
   def button_down(id)
+    case id
+    when Gosu::MsLeft
 
+    end
   end
-
 end
 
 GameWindow.new.show
